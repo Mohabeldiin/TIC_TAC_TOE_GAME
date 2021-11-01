20cd46117d3c44b348c0c515f52d464524f83738
Mohab Mohsen <hobamphab2000@gmail.com>
2021-11-01 18:11:21 +0200

    Init GameUI class

---
+++ PYTHON/Tic_Tac_Toe_OOP/GameUI.py
@@ -0,0 +1,19 @@
+import pygame
+import sys
+pygame.init()
+WIDTH = 600
+HEIGHT = 600
+LINE_WIDTH = 15
+WIN_LINE_WIDTH = 15
+SQUARE_SIZE = 200
+O_RADIUS = 60
+O_WIDTH = 15
+X_WIDTH = 25
+SPACE_FOR_X = 55
+BACK_GROUND_COLOR = (0, 0, 0)
+BACK_GROUND_LINE_COLOR = (111, 111, 111)
+O_COLOR = (254, 132, 255)
+X_COLOR = (219, 88, 136)
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('TIC TAC TOE')
+SCREEN.fill(BACK_GROUND_COLOR)

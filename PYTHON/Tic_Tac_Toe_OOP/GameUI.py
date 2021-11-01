1be5949515299b6c1bc9e7231c577460b92e7610
Mohab Mohsen <hobamphab2000@gmail.com>
2021-11-01 18:35:39 +0200

    adding draw line method

--- PYTHON/Tic_Tac_Toe_OOP/GameUI.py
+++ PYTHON/Tic_Tac_Toe_OOP/GameUI.py
@@ -1,6 +1,7 @@
 import pygame
-import sys
+
 pygame.init()
+
 WIDTH = 600
 HEIGHT = 600
 LINE_WIDTH = 15
@@ -14,6 +15,18 @@ BACK_GROUND_COLOR = (0, 0, 0)
 BACK_GROUND_LINE_COLOR = (111, 111, 111)
 O_COLOR = (254, 132, 255)
 X_COLOR = (219, 88, 136)
+
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('TIC TAC TOE')
 SCREEN.fill(BACK_GROUND_COLOR)
+
+
+def draw_lines():
+    """Draw the screen lines
+    """
+    # horizontal
+    pygame.draw.line(SCREEN, BACK_GROUND_LINE_COLOR, (0, SQUARE_SIZE), (WIDTH, SQUARE_SIZE), LINE_WIDTH)
+    pygame.draw.line(SCREEN, BACK_GROUND_LINE_COLOR, (0, 2 * SQUARE_SIZE), (WIDTH, 2 * SQUARE_SIZE), LINE_WIDTH)
+    # vertical
+    pygame.draw.line(SCREEN, BACK_GROUND_LINE_COLOR, (SQUARE_SIZE, 0), (SQUARE_SIZE, HEIGHT), LINE_WIDTH)
+    pygame.draw.line(SCREEN, BACK_GROUND_LINE_COLOR, (2 * SQUARE_SIZE, 0), (2 * SQUARE_SIZE, HEIGHT), LINE_WIDTH)

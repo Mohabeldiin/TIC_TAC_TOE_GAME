ba272b0abd3e6350efb6688f81841294d0741a22
Mohab Mohsen <hobamphab2000@gmail.com>
2021-11-03 19:30:26 +0200

    fix Board varubels

--- PYTHON/Tic_Tac_Toe_OOP/MODULES/Game.py
+++ PYTHON/Tic_Tac_Toe_OOP/MODULES/Game.py
@@ -1,11 +1,9 @@
 
 import GameUI
+import Board
 class Game():
     """Game class run the Tic Tac Toe game
     """
-    def __init__(self) -> None:
-        pass
-    
 
     def check_win(): 
         """Check if player win
@@ -14,22 +12,22 @@ class Game():
             bool: True if player win
         """
         # vertical win check
-        for col in range(Board.__board_cols):
-            if self.board[0][col] == self.board[1][col] == self.board[2][col] == PLAYER:
+        for col in range(Board.Board.get_BOARD()):
+            if Board.Board.board[0][col] == Board.Board.board[1][col] == Board.Board.board[2][col] == Board.Board.get_player():
                 GameUI.draw_vertical_winning_line(col)
                 return True
 
         # horizontal win check
-        for row in range(Board.__board_rows):
-            if self.board[row][0] == self.board[row][1] == self.board[row][2] == PLAYER:
+        for row in range(Board.Board.get_BOARD()):
+            if Board.Board.board[row][0] == Board.Board.board[row][1] == Board.Board.board[row][2] == Board.Board.get_player():
                 GameUI.draw_horizontal_winning_line(row)
                 return True
 
         # diagonal win check
-        if self.board[2][0] == self.board[1][1] == self.board[0][2] == PLAYER:
+        if Board.Board.board[2][0] == Board.Board.board[1][1] == Board.Board.board[0][2] == Board.Board.get_player():
             GameUI.draw_asc_diagonal()
             return True
-        if self.board[0][0] == self.board[1][1] == self.board[2][2] == PLAYER:
+        if Board.Board.board[0][0] == Board.Board.board[1][1] == Board.Board.board[2][2] == Board.Board.get_player():
             GameUI.draw_desc_diagonal()
             return True
 
@@ -41,6 +39,6 @@ class Game():
         """
         GameUI.SCREEN.fill(GameUI.BACK_GROUND_COLOR)
         GameUI.draw_lines()
-        for row in range(Board.__board_rows):
-            for col in range(Board.__board_cols):
-                Board.board[row][col] = 0
+        for row in range(Board.Board.get_BOARD()):
+            for col in range(Board.Board.get_BOARD()):
+                Board.Board.board[row][col] = 0

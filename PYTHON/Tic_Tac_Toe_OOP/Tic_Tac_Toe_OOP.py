851d9b1336167f2cfbb172825d06ad81bf75fb1a
Mohab Mohsen <hobamphab2000@gmail.com>
2021-11-11 20:39:26 +0200

    Fixing var

--- PYTHON/Tic_Tac_Toe_OOP/Tic_Tac_Toe_OOP.py
+++ PYTHON/Tic_Tac_Toe_OOP/Tic_Tac_Toe_OOP.py
@@ -2,7 +2,7 @@
 from MODULES import Board
 from MODULES import GameUI
 from MODULES import Game
-import sys
+import sys 
 
 
 Game.GameUI.draw_lines()
@@ -17,24 +17,24 @@ while True:
             mouseX = event.pos[0]  # x
             mouseY = event.pos[1]  # y
 
-            clicked_ROW = int(mouseY // GameUI.SQUARE_SIZE)
-            clicked_COL = int(mouseX // GameUI.SQUARE_SIZE)
+            clicked_ROW = int(mouseY // GameUI.GameUI.SQUARE_SIZE)
+            clicked_COL = int(mouseX // GameUI.GameUI.SQUARE_SIZE)
 
             if Game.Board.Board.available_square(clicked_ROW, clicked_COL):
 
-                Board.mark_square(clicked_ROW, clicked_COL, Board.get_player())
-                if Game.Game.check_win(Board.get_player()):
-                    Game.game_over = True
-                PLAYER = PLAYER % 2 + 1
+                Board.Board.mark_square(clicked_ROW, clicked_COL, Board.Board.get_player())
+                if Game.Game.check_win(Board.Board.get_player()):
+                    Game.Game.game_over = True
+                Board.Board.set_player = Board.Board.get_player % 2 + 1
 
-                GameUI.draw_figures()
+                GameUI.GameUI.draw_figures()
                 
 
         if event.type == GameUI.pygame.KEYDOWN:
             if event.key == GameUI.pygame.K_r:
-                Game.restart()
-                PLAYER = PLAYER % 2 + 1
-                Game.game_over = False
+                Game.Game.restart()
+                Board.Board.set_player = Board.Board.get_player % 2 + 1
+                Game.Game.game_over = False
 
     
 
